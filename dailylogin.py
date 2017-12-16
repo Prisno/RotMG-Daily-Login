e6333028f71066ff9eb2240e403f0b3bc2adfc0f
Prisno <34590017+Prisno@users.noreply.github.com>
2017-12-15 22:03:42 -0500

    Add files via upload

---
+++ dailylogin.py
@@ -0,0 +1,29 @@
+# Used to make the script run once every day
+import schedule
+import time
+
+# Used to open the muledump file in a browser and close it
+import webbrowser, os
+import subprocess 
+
+
+def login():
+
+    browser_path = "C:/Program Files (x86)/Mozilla Firefox/firefox.exe" # Path to browser of choice
+    muledump_path = r"" # Your path to muledump
+    
+    # Open the muledump
+    p = subprocess.Popen([browser_path, muledump_path])
+    time.sleep(5)
+    p.kill()
+    
+
+scheduled_time = "22:02" # What time to run the script
+schedule.every().day.at(scheduled_time).do(login)
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)
+    
+
+
